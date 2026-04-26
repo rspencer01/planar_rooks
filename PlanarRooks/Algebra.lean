18153d1d7b4cb3a85d28e814d724e746c0b1712e
Robert Spencer <me@robertandrewspencer.com>
2026-04-26 15:46:25 +0100

    Change Algebra to use a single element struct rather than a alias

--- PlanarRooks/Algebra.lean
+++ PlanarRooks/Algebra.lean
@@ -8,6 +8,10 @@ import PlanarRooks.Finsupp
 import Mathlib.Data.Finsupp.Basic
 import Mathlib.Data.Finsupp.SMulWithZero
 import Mathlib.Data.Fintype.BigOperators
+import Mathlib.LinearAlgebra.Isomorphisms
+import Mathlib.Algebra.Algebra.Hom
+import Mathlib.Algebra.Group.TransferInstance
+import Mathlib.Algebra.Module.TransferInstance
 
 variable {k : Type} [Field k] (δ : k)
 
@@ -22,28 +26,43 @@ formal `k`-linear combinations of planar rook diagrams on `n` strands.
 The multiplication is more complicated and is given by monoid multiplication of diagrams, with
 a factor of `δ` raised to the number of dangling strands after multiplication.
 -/
-def Algebra (n : ℕ) (δ : k) := ((PlanarRook.Diagram n n) → k)
+structure Algebra (n : ℕ) (δ : k) where
+  ofCoeff ::
+  coeff : (PlanarRook.Diagram n n) → k)
 
 namespace Algebra
 
+@[simp]
+def coeffEquiv : Algebra n δ ≃ (PlanarRook.Diagram n n →₀ k) where
+  toFun := Algebra.coeff
+  invFun := Algebra.ofCoeff
+  left_inv _ := rfl
+  right_inv _ := rfl
+
+lemma coeff_inj {x y : Algebra n δ} : x.coeff = y.coeff ↔ x = y := (coeffEquiv δ).injective.eq_iff
+
 @[ext]
 def ext {n : ℕ} {δ : k} {x y : Algebra n δ} :
-    (∀ d : Diagram n n, x d = y d) → x = y := fun h => by simp [funext h]
+    (∀ d : Diagram n n, x.coeff d = y.coeff d) → x = y := fun h => (coeff_inj δ).mp (Finsupp.ext h)
 
-instance : AddCommMonoid (Algebra n δ) := inferInstanceAs (AddCommMonoid (_ → k))
+@[ext]
+def ext' {n : ℕ} {δ : k} {x y : Algebra n δ} :
+    (x.coeff = y.coeff) → x = y := fun h => (coeff_inj δ).mp h
 
-instance : AddCommGroup (Algebra n δ) := inferInstanceAs (AddCommGroup (_ → k))
+@[to_additive]
+noncomputable instance : AddCommGroup (Algebra n δ) := Equiv.addCommGroup (coeffEquiv _)
+instance [DecidableEq k] : DecidableEq  (Algebra n δ) := Equiv.decidableEq (coeffEquiv _)
 
 @[simp]
-theorem zero_coeff (d : Diagram n n) : (0 : Algebra n δ) d = 0 := rfl
+theorem zero_coeff (d : Diagram n n) : (0 : Algebra n δ).coeff d = 0 := rfl
 
 @[simp]
 theorem add_coeff (x₁ x₂ : Algebra n δ) (d : Diagram n n) :
-    (x₁ + x₂) d = (x₁ d) + (x₂ d) := rfl
+    (x₁ + x₂).coeff d = (x₁.coeff d) + (x₂.coeff d) := rfl
 
 /-- The planar rook algebra is a vector space over k.
 -/
-instance : Module k (Algebra n δ) := Pi.module _ _ k
+noncomputable instance instKModule : Module k (Algebra n δ) := Equiv.module k (coeffEquiv _)
 
 /-- The obvious `k` basis of the `PlanarRookAlgebra` is the diagram basis.
 
@@ -55,87 +74,99 @@ Note: This could probably be made more general for arbitrary fintypes.
 -/
 def diagram_basis [DecidableEq k] : Module.Basis (Diagram n n) k (Algebra n δ) := {
     repr := {
-      toFun := equivFunOnFinite.symm
-      invFun := equivFunOnFinite.toFun
+      toFun := (coeffEquiv δ).toFun
+      invFun := (coeffEquiv δ).invFun
       map_add' := fun x y => by ext d; rfl
       map_smul' := fun m x => by ext d; rfl
     }
   }
 
-def single : (PlanarRook.Diagram n n) → k → Algebra n δ := Pi.single
+noncomputable def single : (PlanarRook.Diagram n n) → k → Algebra n δ :=
+  fun a b => (coeffEquiv δ).invFun (Finsupp.single a b)
 
 @[simp]
 def single_apply (d₁ d₂ : PlanarRook.Diagram n n) (c : k) :
-  (Algebra.single δ d₁ c) d₂ = if d₂ = d₁ then c else 0 := Pi.single_apply _ _ _
+  (single δ d₁ c).coeff d₂ = if d₁ = d₂ then c else 0 := Finsupp.single_apply
+
+def diagram_basis_apply [DecidableEq k] (a : Algebra n δ) (d₁ : PlanarRook.Diagram n n) :
+  ((diagram_basis δ).repr x) d = x.coeff d := rfl
+
+def diagram_basis_is_single [DecidableEq k] (d₁ : PlanarRook.Diagram n n) :
+  (diagram_basis δ d₁) = single δ d₁ 1 := by
+    ext d₂
+    simp only [diagram_basis, single_apply]
+    by_cases h : d₁ = d₂
+    · simp [h]
+    · simp [h]
+
+theorem smul_inner (f : Diagram n n →₀ k) (c : k) :
+  c • ((coeffEquiv δ).invFun f) = (coeffEquiv δ).invFun (c • f) :=  rfl
+
+theorem smul_inner' (a : Algebra n δ) (c : k) (d : Diagram n n) :
+  (c • a).coeff d = c * (a.coeff d) :=  rfl
 
 @[simp]
 theorem smul_single (d₁ : PlanarRook.Diagram n n) (c₁ c₂ : k) :
   c₁ • (Algebra.single δ d₁ c₂) = Algebra.single δ d₁ (c₁ * c₂) := by
-    unfold single
-    rw [←Pi.single_smul]
-    simp only [smul_eq_mul]
+    ext d
+    rw[smul_inner']
+    simp
 
 @[simp]
 theorem smul_single' (d₁ : PlanarRook.Diagram n n) (c : k) :
   c • (Algebra.single δ d₁ 1) = Algebra.single δ d₁ c := by
     simp only [Algebra.smul_single δ d₁ c 1, mul_one]
 
+@[simp]
+theorem sum_coeff (f : ι → Algebra n δ) (s : Finset ι) :
+  (∑ i ∈ s, f i).coeff = ∑ i ∈ s, (f i).coeff := by sorry
+
 theorem sum_single (x : Algebra n δ) :
-  x = ∑ d : (PlanarRook.Diagram n n), Algebra.single δ d (x d) := by
-    ext m
-    rw [Finset.univ.sum_apply m]
-    simp [single_apply]
+  x = ∑ d : (PlanarRook.Diagram n n), Algebra.single δ d (x.coeff d) := by
+    ext d'
+    simp[sum_coeff _ _]
 
 theorem add_single (d : PlanarRook.Diagram n n) (c₁ c₂ : k) :
   Algebra.single δ d (c₁ + c₂) = Algebra.single δ d c₁ + Algebra.single δ d c₂ := by
     ext x
     simp only [single_apply, add_coeff]
-    by_cases h: x = d
+    by_cases h: d = x
     · simp only [h, ↓reduceIte]
     · simp only [h, ↓reduceIte, add_zero]
+
+noncomputable def one : Algebra n δ := single δ (Diagram.id n) 1
+
+noncomputable instance hasOne : One (Algebra n δ) := ⟨one δ⟩
+
+theorem one_def : (1 : Algebra n δ) = one δ := rfl
+
+def one_apply (d : Diagram n n) : (1 : Algebra n δ).coeff d = if Diagram.id n = d then 1 else 0 := by
+    simp[one_def, one]
+
+def one_is : (1 : Algebra n δ) = single δ (Diagram.id n) 1 := rfl
+
+noncomputable instance addGroupWithOne : AddGroupWithOne (Algebra n δ) := {}
 
 /-- Multiplication in the planar rook algebra depends on a paramter δ
 
 This paramter is raised to the exponent of the number of dangling strands
 after monoid multiplication. -/
-def mul (x y : Algebra n δ) : Algebra n δ :=
+noncomputable def mul (x y : Algebra n δ) : Algebra n δ :=
   ∑ d₁ : (Diagram n n), ∑ d₂ : (Diagram n n),
-    ((x d₁) * (y d₂)) • (Algebra.single δ (d₁ * d₂) (δ ^ Monoid.mul_exponent d₁ d₂))
+    ((x.coeff d₁) * (y.coeff d₂)) • (Algebra.single δ (d₁ * d₂) (δ ^ Monoid.mul_exponent d₁ d₂))
 
-def one : Algebra n δ := single δ (Diagram.id n) 1
-
-def one_apply (d : Diagram n n) : (one δ) d = if Diagram.id n = d then 1 else 0 := by
-    simp only [one, single_apply]
-    by_cases h : Diagram.id n = d
-    · simp [h]
-    · rw [eq_comm] at h
-      simp only [h, ↓reduceIte, right_eq_ite_iff, zero_ne_one, imp_false]
-      rw [eq_comm] at h
-      exact h
-
-instance : Mul (Algebra n δ) := ⟨mul δ⟩
+noncomputable instance : Mul (Algebra n δ) := ⟨mul δ⟩
 
 theorem mul_def (x y : Algebra n δ) :
   x * y = ∑ d₁ : (Diagram n n), ∑ d₂ : (Diagram n n),
-    ((x d₁) * (y d₂)) • (single δ (d₁ * d₂) (δ ^ Monoid.mul_exponent d₁ d₂)) := rfl
+    ((x.coeff d₁) * (y.coeff d₂)) • (single δ (d₁ * d₂) (δ ^ Monoid.mul_exponent d₁ d₂)) := rfl
 
 theorem mul_apply (x y : Algebra n δ) :
-  (x * y) m = ∑ d₁, ∑ d₂,
-    if d₁ * d₂ = m then (x d₁) * (y d₂) * (δ ^ Monoid.mul_exponent d₁ d₂) else 0 := by
-  rw [mul_def]
-  conv => {
-    lhs
-    rw [Fintype.sum_apply m]
-    arg 2
-    simp [Finset.sum_apply m (s := Finset.univ), smul_single δ]
-  }
-  apply Finset.sum_congr rfl
-  intro x₁ hx₁
-  apply Finset.sum_congr rfl
-  intro x₂ hx₂
-  simp[eq_comm]
+  (x * y).coeff m = ∑ d₁, ∑ d₂,
+    if d₁ * d₂ = m then (x.coeff d₁) * (y.coeff d₂) * (δ ^ Monoid.mul_exponent d₁ d₂) else 0 := by
+  simp[mul_def]
 
-instance nonUnitalNonAssocSemiring : NonUnitalNonAssocSemiring (Algebra n δ) := {
+noncomputable instance nonUnitalNonAssocSemiring : NonUnitalNonAssocSemiring (Algebra n δ) := {
   left_distrib := fun a b c => by
     ext d
     simp only [add_coeff, mul_apply, ←Finset.univ.sum_add_distrib]
@@ -164,16 +195,16 @@ instance nonUnitalNonAssocSemiring : NonUnitalNonAssocSemiring (Algebra n δ) :=
 
 theorem mul_single (x : Algebra n δ) (d₁ : Diagram n n) (c : k) :
   x * (single δ d₁ c) =
-    ∑ d₂ : (Diagram n n),  (x d₂) • (single δ (d₂ * d₁) (c * (δ ^ Monoid.mul_exponent d₂ d₁))) := by
-  simp only [mul_def, single_apply, mul_ite, mul_zero, ite_smul, smul_single,
-    zero_smul, Finset.sum_ite_eq', Finset.mem_univ, ↓reduceIte]
+    ∑ d₂ : (Diagram n n),  (x.coeff d₂) • (single δ (d₂ * d₁) (c * (δ ^ Monoid.mul_exponent d₂ d₁))) := by
+  simp only [mul_def, single_apply, mul_ite, mul_zero, ite_smul, smul_single, zero_smul,
+    Finset.sum_ite_eq, Finset.mem_univ, ↓reduceIte]
   congr
   ext x
   ring_nf
 
 theorem single_mul (x : Algebra n δ) (d₁ : Diagram n n) (c : k) :
   (single δ d₁ c) * x =
-    ∑ d₂ : (Diagram n n), (x d₂) • (single δ (d₁ * d₂) (c * (δ ^ Monoid.mul_exponent d₁ d₂))) := by
+    ∑ d₂ : (Diagram n n), (x.coeff d₂) • (single δ (d₁ * d₂) (c * (δ ^ Monoid.mul_exponent d₁ d₂))) := by
   rw [mul_def]
   conv => {
     lhs
@@ -191,10 +222,9 @@ theorem single_mul (x : Algebra n δ) (d₁ : Diagram n n) (c : k) :
     ext d₁
     simp [Finset.univ.sum_ite_eq']
   }
-  simp only [Finset.univ.sum_ite_eq', Finset.mem_univ, ↓reduceIte]
+  simp only [Finset.sum_ite_eq, Finset.mem_univ, ↓reduceIte, smul_single]
   apply Finset.sum_congr rfl
   intro x₁ hx₁
-  simp [smul_single]
   ring_nf
 
 theorem mul_single_single (d₁ d₂ : Diagram n n) (c₁ c₂ : k) :
@@ -208,7 +238,7 @@ theorem mul_single_single (d₁ d₂ : Diagram n n) (c₁ c₂ : k) :
 We can now show that multiplication is associative. That is, we have a non-unital semiring structure
 on the planar rook algebra. We will later show that it is unital, and hence a ring.
 -/
-instance nonUnitalSemiring :
+noncomputable instance nonUnitalSemiring :
     NonUnitalSemiring (Algebra n δ) := {
   mul_assoc := fun a b c => by
     rw [sum_single _ a]
@@ -240,19 +270,12 @@ instance nonUnitalSemiring :
     ring_nf
 }
 
-instance hasOne : One (Algebra n δ) := ⟨one δ⟩
 
-theorem one_def : (1 : Algebra n δ) = one δ := rfl
 
-def one_is : (1 : Algebra n δ) = single δ (Diagram.id n) 1 := rfl
-
-instance addGroupWithOne : AddGroupWithOne (Algebra n δ) := {}
-
-instance is_semiring :
+noncomputable instance is_semiring :
     Semiring (Algebra (k:=k) n δ) := {
   one_mul := fun a => by
     ext d
-    rw [one_def]
     rw [mul_apply]
     simp only [one_apply, ite_mul, one_mul, zero_mul]
     conv => {
@@ -283,10 +306,8 @@ instance is_semiring :
   mul_one := by
     intro a
     ext d
-    rw [one_def]
     rw [mul_apply]
-    simp only [one_apply, mul_ite, mul_one, mul_zero, ite_mul,
-      zero_mul]
+    simp only [one_apply, mul_ite, mul_one, mul_zero, ite_mul, zero_mul]
     conv => {
       lhs
       arg 2
@@ -319,7 +340,7 @@ instance is_semiring :
     simp [Monoid.mul_exponent_eq_zero_of_id]
 }
 
-def single_one_ring_hom : k →+* Algebra n δ :=
+noncomputable def single_one_ring_hom : k →+* Algebra n δ :=
   {
     toFun := fun c => c • (1 : Algebra n δ)
     map_one' := one_smul _ _
@@ -331,7 +352,7 @@ def single_one_ring_hom : k →+* Algebra n δ :=
     map_add' := fun x y => by simp [one_is, add_single]
   }
 
-instance is_algebra (δ : k) :
+noncomputable instance is_algebra (δ : k) :
     _root_.Algebra k (PlanarRook.Algebra n δ) := {
   algebraMap := single_one_ring_hom δ,
   commutes' := fun r x => by
@@ -377,11 +398,10 @@ instance is_algebra (δ : k) :
     intro x₁ hx₁
     ext d₂
     simp only [single_apply, smul_single δ]
-    by_cases h : d₂ = x₁
+    by_cases h : x₁ = d₂
     · simp only [h, ↓reduceIte]
-      rw [Pi.smul_apply]
-      simp
-      simp [mul_comm r (x x₁)]
+      simp only [smul_inner']
+      exact mul_comm (G:=k) _ _
     · simp [h]
 }
 
@@ -393,105 +413,112 @@ it is not zero.
 This is to say, there are only "two" planar rook algebras, the one where δ = 0 and the one where δ
 is nonzero (and might as well be 1).
 -/
-def parameter_independence (n : ℕ) (δ₁ : k) (δ₁_nonzero : δ₁ ≠ 0) :
+noncomputable def parameter_independence (n : ℕ) (δ₁ : k) (δ₁_nonzero : δ₁ ≠ 0) [DecidableEq k] :
     (Algebra n δ₁) ≃ₐ[k] (Algebra n (1 : k)) := {
-      toFun := fun x => ∑ d , single 1 d (x d * (δ₁^ ((n : ℤ) - ↑d.through_index)))
-      invFun := fun y => ∑ d, single δ₁ d (y d / (δ₁^ ((n : ℤ) - ↑d.through_index)))
+      toFun := fun x => ∑ d , ((x.coeff d) * (δ₁^ ((n : ℤ) - ↑d.through_index))) • (diagram_basis 1 d)
+      invFun := fun y => ∑ d, (y.coeff d / (δ₁^ ((n : ℤ) - ↑d.through_index))) • (diagram_basis δ₁ d)
       left_inv := by
         intro x
-        simp only [Finset.univ.sum_apply, single_apply, Finset.sum_ite_eq, Finset.mem_univ,
-          ↓reduceIte]
+        simp only [diagram_basis_is_single, smul_single, mul_one]
         conv => {
           lhs
           arg 2
           intro x₁
           arg 3
-          rw [←Int.ofNat_sub (Diagram.through_index_le_left _)]
           simp [δ₁_nonzero]
+          rw[←mul_div]
+          rw[div_self (zpow_ne_zero (n - x₁.through_index) δ₁_nonzero)]
+          simp
         }
         rw [←sum_single δ₁ x]
       right_inv := by
         intro x
-        simp only [Finset.univ.sum_apply, single_apply, Finset.sum_ite_eq, Finset.mem_univ,
-          ↓reduceIte]
+        simp only [diagram_basis_is_single, smul_single, mul_one]
         conv => {
           lhs
           arg 2
           intro x₁
           arg 3
-          rw [←Int.ofNat_sub (Diagram.through_index_le_left _)]
           simp [δ₁_nonzero]
+          rw[div_mul]
+          rw[div_self (zpow_ne_zero (n - x₁.through_index) δ₁_nonzero)]
+          simp
         }
         rw [←sum_single 1 x]
       map_mul' := by
         intro x y
-        simp only [Finset.sum_mul_sum, mul_single_single]
+        simp only [diagram_basis_is_single, smul_single]
+        simp [Finset.sum_mul_sum, mul_single_single]
         ring_nf
         simp only [mul_assoc]
         apply ext
         intro d
-        simp only [Finset.sum_apply d, single_apply]
-        conv => {
-          lhs
-          rw [Finset.sum_ite_eq_of_mem (h:=Finset.mem_univ d)]
-          rw [mul_apply]
-          rw [Finset.sum_mul]
-          arg 2
-          ext x₁
-          rw [Finset.sum_mul]
-        }
-        apply Finset.sum_congr rfl
-        intro x₁ hx₁
-        apply Finset.sum_congr rfl
-        intro x₂ hx₂
-        rw [ite_mul]
-        by_cases h : d = x₁ * x₂
-        · simp only [h, ↓reduceIte]
-          rw [←zpow_natCast]
-          rw [←Int.ofNat_sub (Diagram.through_index_le_left x₁)]
-          rw [←Int.ofNat_sub (Diagram.through_index_le_left x₂)]
-          ring_nf
-          conv => {
-            rhs
-            rw [mul_assoc]
-            arg 2
-            rw [←zpow_add' (Or.inl δ₁_nonzero)]
-          }
-          simp only [mul_assoc, ←zpow_add' (Or.inl δ₁_nonzero)]
-          simp only [Monoid.mul_exponent, Finset.compl_union, Diagram.through_degree_of_mul,
-            mul_eq_mul_left_iff]
-          apply Or.inl
-          apply Or.inl
-          rw [Int.ofNat_sub (Diagram.through_index_le_left _)]
-          rw [Int.ofNat_sub (Diagram.through_index_le_left _)]
-          rw [←Finset.compl_union]
-          rw [Finset.cast_card_inter]
-          rw [Finset.compl_eq_univ_sdiff]
-          rw [Finset.cast_card_sdiff (Finset.subset_univ _)]
-          simp [Finset.card_univ]
-          ring_nf
-          rw[Diagram.through_index_eq_right]
-          rw[Diagram.through_index_eq_left]
-
-        · simp [h, eq_comm]
+        sorry
+        -- conv => {
+        --   lhs
+        --   simp only [sum_coeff]
+        --   simp [Finset.sum_apply (a:=d) (s:=Finset.univ)]
+        --   rw [Finset.sum_ite_eq_of_mem (h:=Finset.mem_univ d)]
+        --   rw [mul_apply]
+        --   rw [Finset.sum_mul]
+        --   arg 2
+        --   ext x₁
+        --   rw [Finset.sum_mul]
+        -- }
+        -- apply Finset.sum_congr rfl
+        -- intro x₁ hx₁
+        -- apply Finset.sum_congr rfl
+        -- intro x₂ hx₂
+        -- rw [ite_mul]
+        -- by_cases h : d = x₁ * x₂
+        -- · simp only [h, ↓reduceIte]
+        --   rw [←zpow_natCast]
+        --   rw [←Int.ofNat_sub (Diagram.through_index_le_left x₁)]
+        --   rw [←Int.ofNat_sub (Diagram.through_index_le_left x₂)]
+        --   ring_nf
+        --   conv => {
+        --     rhs
+        --     rw [mul_assoc]
+        --     arg 2
+        --     rw [←zpow_add' (Or.inl δ₁_nonzero)]
+        --   }
+        --   simp only [mul_assoc, ←zpow_add' (Or.inl δ₁_nonzero)]
+        --   simp only [Monoid.mul_exponent, Finset.compl_union, Diagram.through_degree_of_mul,
+        --     mul_eq_mul_left_iff]
+        --   apply Or.inl
+        --   apply Or.inl
+        --   rw [Int.ofNat_sub (Diagram.through_index_le_left _)]
+        --   rw [Int.ofNat_sub (Diagram.through_index_le_left _)]
+        --   rw [←Finset.compl_union]
+        --   rw [Finset.cast_card_inter]
+        --   rw [Finset.compl_eq_univ_sdiff]
+        --   rw [Finset.cast_card_sdiff (Finset.subset_univ _)]
+        --   simp [Finset.card_univ]
+        --   ring_nf
+        --   rw[Diagram.through_index_eq_right]
+        --   rw[Diagram.through_index_eq_left]
+        -- · simp [h, eq_comm]
       map_add' := by
         intro x y
+        simp only [diagram_basis_is_single, smul_single]
         simp only [add_coeff, ← Finset.sum_add_distrib]
         apply Finset.sum_congr rfl
         intro x₁ hx₁
         rw [←add_single 1 x₁, ←add_mul]
+        ring_nf
       commutes' := by
         intro r
+        simp only [diagram_basis_is_single, smul_single]
         simp only [algebra_map, single_one_ring_hom, one_is,
           RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
         ext x
         simp [Finset.univ.sum_apply x, single_apply]
-        by_cases h : x = (PlanarRook.Diagram.id n)
-        · simp [h, PlanarRook.Diagram.through_index_of_id]
+        by_cases h : (PlanarRook.Diagram.id n) = x
+        · simp [h.symm, PlanarRook.Diagram.through_index_of_id]
         · simp [h]
     }
 
-instance ringConGen : Ring (Algebra n δ) := {is_semiring _, addGroupWithOne _ with}
+noncomputable instance ringConGen : Ring (Algebra n δ) := {is_semiring _, addGroupWithOne _ with}
 
 variable [DecidableEq k]
 
@@ -504,6 +531,18 @@ theorem diagram_basis_mul (a b : PlanarRook.Diagram n n) :
     rw[←smul_single' (c:= δ ^ _)] at q
     unfold single at q
     simp[q]
+    rw[mul_def]
+    simp
+    conv_lhs => {
+      arg 2
+      ext x
+      arg 2
+      ext y
+      arg 3
+      rw[Finsupp.single_apply]
+      rw[Finsupp.single_apply]
+    }
+    sorry
 
 def diagram_basis' :
   Module.Basis
